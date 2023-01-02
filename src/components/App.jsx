037e2dd0064148a005158cee8d3e3202b393da6c
Maryna-Korbet <m22v86k@gmail.com>
2023-01-03 01:27:59 +0200

    added LoginForm.js

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,5 @@
+import { LoginForm } from "./LoginForm/LoginForm";
+
 export const App = () => {
   return (
     <div
@@ -10,7 +12,7 @@ export const App = () => {
         color: '#010101'
       }}
     >
-      React homework template
+      <LoginForm />
     </div>
   );
 };
