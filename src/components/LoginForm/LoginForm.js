3466e4a87529686a9fa6fae8ae6c6810025c7211
Maryna-Korbet <m22v86k@gmail.com>
2023-01-03 02:29:49 +0200

    added ErrorMessage

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -1,4 +1,4 @@
-import { Formik, Form, Field } from 'formik';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 import css from 'components/LoginForm/LoginForm.module.css';
 import * as yup from 'yup';
 
@@ -29,11 +29,13 @@ export const LoginForm = () => {
             <label htmlFor="login">
                 Login
                     <Field type="text" name="login" className={css.input} />
+                    <ErrorMessage name="login" component="div"/>
                 </label>
                 
             <label htmlFor="password">
                 Password
-                <Field type="password" name="password" className={css.input}/>
+                    <Field type="password" name="password" className={css.input} />
+                    <ErrorMessage name="password" component="div" />
                 </label>
                 
             <button type="submit">Submit</button>
